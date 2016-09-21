28244baadd8b4b3b6b2a9cededcb88678ba61e64
Josh Zhang <josh.zhang91@gmail.com>
2016-09-21 10:09:23 -0700

    basic codeigniter stuff

---
+++ application/models/User.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User extends CI_Controller {
+
+	public function login() {
+
+	}
+
+	public function register() {
+		
+	}
+
+
+
+}
