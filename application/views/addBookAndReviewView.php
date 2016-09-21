fb1adc3e8216a086fad8a68d16dedfe1a72f31bc
Timothy Lo <tim7643@gmail.com>
2016-09-21 11:02:43 -0700

    adding addbookandreview page adding addbookandreview page

---
+++ application/views/addBookAndReviewView.php
@@ -0,0 +1,61 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title></title>
+	<!-- Google Icons -->
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+	<!-- Jquery Theme -->
+	<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/hot-sneaks/jquery-ui.css">
+	<!-- Materialize CSS -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
+	<!-- Personal CSS -->
+	<link rel="stylesheet" href="assets/css/style.css">
+
+
+	<!-- Less -->
+	<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/2.7.1/less.min.js"></script>
+
+
+	<!-- Jquery --> 
+	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+
+	<!-- Materialize JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
+
+	<script>
+		
+	</script>
+</head>
+<body>
+	<div>
+		<a href="">Home</a><a href="">Logout</a>
+	</div>
+	<div>
+		<h3>Add a New Book Title and a Review:</h3>
+	</div>
+	<div>
+		<form>
+			<p>Book Title: <input type="" name=""></p>
+			<p>Author:</p>
+			<p>Choose from a list: <input type="" name=""></p>
+			<p>Or add a new author:</p>
+			<p>Review <textarea></textarea>
+			<p>Rating: <input type="" name="">
+			<option>5</option>
+			<option>4</option>
+			<option>3</option>
+			<option>2</option>
+			<option>1</option>
+			stars.
+			<input type="submit" name="Add Book and Review">
+		</form>
+	</div>
+</body>
+</html>
