add2326e32a0d38739b034ae086449e92c3e70de
Timothy Lo <tim7643@gmail.com>
2016-09-21 11:50:15 -0700

    worked on page 4 a little

--- application/views/bookReviewView.php
+++ application/views/bookReviewView.php
@@ -34,34 +34,56 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	</script>
 </head>
 <body>
-	<div>
-		<a href="">Home</a><a href="">Logout</a>
-	</div>
-	<div>
-		<p> [phpcontentTITLE]</p>
-		<p> [phpcontentAUTHOR] </p>
-	</div>
-	<div>
-		<h3>Reviews</h3>
-		<p>[rating in stars]</p>  <!-- foreach -->
-		<p>[user] " says " [comment]</p>
-		<p>[date posted]
-			<p><a href="">Delete this Review</a></p> <!-- [if this review is user's review, show link] -->
+	<nav>
+		<div class="container">
+			<div class="nav-wrapper">
+				<h5 class="brand-logo">Welcome, Josh </h5>
+				<ul id="nav-mobile" class="right">
+					<li><a>Home</a></li>
+					<li><a>Logout</a></li>
+				</ul>
+			</div>
 		</div>
-		<div>
-			<h4>Add a Review:</h4>
-			<div class="input-field col s12">
-				<textarea id="textarea1" class="materialize-textarea"></textarea>
-				<label for="textarea1">Review</label>
-			</div>
-			<p>Rating: <input type="" name="">
-				<option>5</option>
-				<option>4</option>
-				<option>3</option>
-				<option>2</option>
-				<option>1</option>
-				stars.
-				<input type="submit" name="Submit Review">
-			</div>
-		</body>
-		</html>
+	</nav>
+	<div>
+		<p>Divergent</p>
+		<p>Author: Veronica Roth </p>
+	</div>
+	<div class="col s6">
+		<h3>Reviews</h3>
+		<table class="bordered">
+			<thead>
+				<tr>
+					<th data-field="id">Name</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>Rating</td>
+				</tr>
+				<tr>
+					<td>Rating</td>
+				</tr>
+				<tr>
+					<td>Rating</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+	<div>
+		<h4>Add a Review:</h4>
+		<div class="input-field col s12">
+			<textarea id="textarea1" class="materialize-textarea"></textarea>
+			<label for="textarea1">Review</label>
+		</div>
+		<p>Rating: <input type="" name="">
+			<option>5</option>
+			<option>4</option>
+			<option>3</option>
+			<option>2</option>
+			<option>1</option>
+			stars.
+			<input type="submit" name="Submit Review">
+		</div>
+	</body>
+	</html>
