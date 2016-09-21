54643ce238ceef80a3d3fa6a0812cbb16714407b
Josh Zhang <josh.zhang91@gmail.com>
2016-09-21 10:44:46 -0700

    added style.css file. created welcome view with login and registration. made basic changes to welcome controller.

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,14 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('welcomeView');
+	}
+
+	public function register() {
+
+	}
+
+	public function login() {
+		
 	}
 }
